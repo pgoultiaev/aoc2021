205e710829e206254e286aefbbfe320ab7d9119b
Pavel Goultiaev <pavel.goultiaev@gmail.com>
2022-01-12 09:30:28 +0100

    minor formatting for day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -7,7 +7,7 @@ fun main() {
                 when (k) {
                     6 -> mMap[6] = input.getOrDefault(7, 0) + input.getOrDefault(0, 0)
                     8 -> mMap[8] = input.getOrDefault(0, 0)
-                    else -> mMap[k] = input.getOrDefault(k+1, 0)
+                    else -> mMap[k] = input.getOrDefault(k + 1, 0)
                 }
             }
             doGrowthRound(mMap, roundsLeft - 1)
@@ -18,7 +18,10 @@ fun main() {
         return doGrowthRound(input, rounds).values.reduce { acc, i -> acc + i }
     }
 
-    val input = readInput("06").first().split(",").map { s -> s.toInt() }.groupBy { it }.mapValues { (_, fish) -> fish.size.toLong() }
+    val input = readInput("06").first().split(",")
+        .map { s -> s.toInt() }
+        .groupBy { it }
+        .mapValues { (_, fish) -> fish.size.toLong() }
     println("1: ${solve(input, 80)}")
     println("2: ${solve(input, 256)}")
 }
